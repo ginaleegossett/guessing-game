71ae87f75417c1d136e37d6b0396e431f42ac357
Gina Lee Gossett <ginaleegossett@Ginas-MacBook-Pro.local>
2015-09-30 13:35:57 -0700

    question 3 if/else statement added in, question to 'Yes' answer fixed

--- game.js
+++ game.js
@@ -7,15 +7,22 @@ if (question1.toUpperCase() === 'YES') {
 }
 
 var question2 = prompt('Is autumn the best season?');
-if (question2.toUpperCase() === ' YES') {
+
+if (question2.toUpperCase() === 'YES') {
 	alert('You are correct! Autumn is the best season!');
 } else {
 	alert('Sorry, you are wrong. Autumn means sweaters and boots and Pumpkin Spiced Lattes!');
 }
 var question3 = prompt('Is Harry Potter your favorite book series?');
 
+if (question3.toUpperCase() === 'YES') {
+	alert('You are a pretty cool person!');
+} else {
+	alert('It is not? You need to rethink your answer.');
+}
+
 console.log('Does user live in Montana ' + question1);
-console.log('Is user favorite color blue? ' + question2);
+console.log('Is autumn the best season? ' + question2); 		
 console.log('Is Harry Potter favorite book series? ' + question3);
 
 
