1d38542db2b9dc75a2b97c2386aa6c9550344e53
Gina Lee Gossett <ginaleegossett@Ginas-MacBook-Pro.local>
2015-09-30 14:18:04 -0700

    adds summary

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
+var i = 0
+
+
 var question1 = prompt('Do you live in Montana?');
 
 if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
 	alert('Lucky you! What a beautiful place to live!');
+	i++;
 } else {
 	alert('No? You are missing out!');
 }
@@ -10,6 +14,7 @@ var question2 = prompt('Is autumn the best season?');
 
 if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
 	alert('You are correct! Autumn is the best season!');
+	i++;
 } else {
 	alert('Sorry, you are wrong. Autumn means sweaters and boots and Pumpkin Spiced Lattes!');
 }
@@ -17,9 +22,12 @@ var question3 = prompt('Is Harry Potter your favorite book series?');
 
 if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
 	alert('Yes! You are a pretty cool person!');
+	i++;
 } else {
 	alert('It is not? You need to rethink your answer.');
 }
+
+alert('Congratulations! You got ' + i + ' answers correct! You are pretty awesome!');
 
 console.log('Does user live in Montana ' + question1);
 console.log('Is autumn the best season? ' + question2); 		
