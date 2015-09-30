6e49086e23ae1215bef811ec8b3f1d99530bbee6
Gina Lee Gossett <ginaleegossett@Ginas-MacBook-Pro.local>
2015-09-30 13:27:20 -0700

    question 2 if/else statement added in

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
 var question1 = prompt('Do you live in Montana?');
 
 if (question1.toUpperCase() === 'YES') {
-	alert('What a beautiful place to live!');
+	alert('Lucky you! What a beautiful place to live!');
 } else {
-	alert('You are missing out!');
+	alert('No? You are missing out!');
 }
 
-var question2 = prompt('Is your favorite color blue?');
+var question2 = prompt('Is autumn the best season?');
+if (question2.toUpperCase() === ' YES') {
+	alert('You are correct! Autumn is the best season!');
+} else {
+	alert('Sorry, you are wrong. Autumn means sweaters and boots and Pumpkin Spiced Lattes!');
+}
 var question3 = prompt('Is Harry Potter your favorite book series?');
 
 console.log('Does user live in Montana ' + question1);
