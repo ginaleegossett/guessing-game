d7c06bfa407fb15ccefbb7c99758578416aad2de
Mike <mikekh@uw.edu>
2015-10-02 15:48:04 -0700

    Added CSS file: Added Red Green Ans

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ var imgOne = document.getElementById("imgOne");
 var imgTwo = document.getElementById("imgTwo");
 var imgThree = document.getElementById("imgThree");
 
+
+
 var questions = ['Do you live in Montana?', 'Is autumn the best season?', 'Is Harry Potter your favorite book series?']
 var answers = ['YES', 'Y','NO', 'N']
 var userScore = 0
@@ -17,9 +19,11 @@ function ques1() {
   if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[1]) {
   	one.innerHTML='Lucky you! What a beautiful place to live!';
     imgOne.innerHTML="<img src=img/montana.jpeg width=200 />";
+    one.className = "right";
   	userScore++;
   } else {
   	one.innerHTML='No? You are missing out!';
+    one.className = "wrong";
   }
   console.log('Does user live in Montana ' + question1);
 }
@@ -30,9 +34,11 @@ function ques2() {
   if (question2.toUpperCase() === answers[0] || question2.toUpperCase() === answers[1]) {
   	two.innerHTML='You are correct! Autumn is the best season!';
     imgTwo.innerHTML="<img src=img/autumn.jpg width=200 />";
+    two.className = "right";
   	userScore++;
   } else {
   	two.innerHTML='Sorry, you are wrong. Autumn means sweaters and boots and Pumpkin Spiced Lattes!';
+    two.className = "wrong";
   }
   console.log('Is autumn the best season? ' + question2);
 }
@@ -43,16 +49,18 @@ function ques3() {
   if (question3.toUpperCase() === answers[0] || question3.toUpperCase() === answers[1]) {
   	three.innerHTML='Yes! You are a pretty cool person!';
     imgThree.innerHTML="<img src=img/harrypotterbooks.jpg width=200 />";
+    three.className = "right";
   	userScore++;
   } else {
   	three.innerHTML='It is not? You need to rethink your answer.';
+    three.className = "wrong";
   }
   console.log('Is Harry Potter favorite book series? ' + question3);
 }
 
-ques1();
-window.setTimeout(ques2, 2000);
-window.setTimeout(ques3, 2000);
+ ques1();
+ window.setTimeout(ques2, 2000);
+ window.setTimeout(ques3, 2000);
 
 // alert('Thanks for playing! You got ' + userScore + ' answers correct! You are pretty awesome!');
 
